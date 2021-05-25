3e2d2393b46f09cfae95d33d1a208e23262b42f0
BenigneDe <benigne.demetz@etu.univ-amu.fr>
2021-05-25 16:24:17 +0200

    triangle area 3.1 test 1, 2 et 3 foncitonnels

--- src/test/java/fr/univ_amu/iut/exercice3_1/TriangleAreaTest.java
+++ src/test/java/fr/univ_amu/iut/exercice3_1/TriangleAreaTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
-@Disabled
+
 public class TriangleAreaTest {
     private TriangleArea triangleArea;
     private PrintStream out;
@@ -20,13 +20,13 @@ public class TriangleAreaTest {
         System.setOut(out);
     }
 
-    @Disabled
+
     @Test
     public void should_an_empty_triangle_have_a_zero_area() throws Exception {
         assertThat(triangleArea.getArea()).isZero();
     }
 
-    @Disabled
+
     @Test
     public void should_an_unity_triangle_have_a_0_5_area() throws Exception {
         triangleArea.setP1(0, 0);
@@ -36,7 +36,7 @@ public class TriangleAreaTest {
     }
 
 
-    @Disabled
+
     @Test
     public void should_triangle1_have_a_correct_area() throws Exception {
         triangleArea.setP1(0, 0);
